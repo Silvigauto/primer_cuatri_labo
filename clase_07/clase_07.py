52f9ceea8fcfc03d84ec84d2ce8595022c6cf0fc
Silvina Gauto <silvigauto02@gmail.com>
2023-08-24 11:22:39 -0300

    clase 7 strings

---
+++ clase_07/clase_07.py
@@ -0,0 +1,11 @@
+#strip --> saca los espacios en blanco
+mi_cadena = '           hola'
+mi_cadena = mi_cadena.strip()
+
+#upper --> convierte a mayusculas
+#lower --> convierte todo a minuscula
+#capitalize --> convierte a mayuscula la primer letra dentro de la cadena
+
+
+#replace --> reemplaza una cadena por otra, recibe dos parametros (palabra a reemplazar, palabra )
+mi_cadena.replace('mundo', 'zzz')
